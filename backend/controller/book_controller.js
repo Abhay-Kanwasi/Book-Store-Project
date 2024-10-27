3b91505c1ae6888a47b7d4a8a142e7d71609f3af
Abhay-Kanwasi <abhaykanwasi123@gmail.com>
2024-10-27 21:08:16 +0530

    Changes:
    update book by id api added

--- backend/controller/book_controller.js
+++ backend/controller/book_controller.js
@@ -3,14 +3,13 @@ import { Book } from "../models/book_model.js";
 
 class BookController {
     static main_page = async (request, response) => {
-        return response.status(234).send('Welcome to book store project')
+        return response.status(234).send('Welcome to book store project');
     };
 
     static create_book = async (request, response) => {
         try {
-            console.log(request.body)
             if ( !request.body.title || !request.body.author || !request.body.publishYear ) {
-                return response.status(400).send({ message: 'Send all required fields: title, author, publishYear' })
+                return response.status(400).send({ message: 'Send all required fields: title, author, publishYear' });
             }
 
             const newBook = {
@@ -37,7 +36,7 @@ class BookController {
             })
         }
         catch (error) {
-            console.log(error.message)
+            console.log(error.message);
             response.status(500).send({ message: error.message });
         }
     }
@@ -46,11 +45,29 @@ class BookController {
         try {
             const { id } = request.params;
             const book = await Book.findById(id);
-            return response.status(200).json(book)
+            return response.status(200).json(book);
         }
         catch (error) {
             console.log(error.message);
             return response.status(500).send({ message: error.message });
+        }
+    }
+
+    static update_book = async (request, response) => {
+        try {
+            if ( !request.body.title || !request.body.author || !request.body.publishYear ) {
+                return response.status(400).send({ message: "Send all required fields: title, author, publishYear"});
+            }
+            const { id } = request.params;
+            const result = await Book.findByIdAndUpdate(id, request.body);
+            if (!result) {
+                return response.status(404).json({ message: "Book not found"});
+            }
+            return response.status(200).send({ message: "Book updated successfully" });
+        }
+        catch (error) {
+            console.log(error.message);
+            response.status(500).send({ message: error.message });
         }
     }
 }
--- backend/routes/bookRoutes.js
+++ backend/routes/bookRoutes.js
@@ -6,5 +6,6 @@ router.get('/', BookController.main_page)
 router.post('/create_book', BookController.create_book)
 router.get('/get_all_books', BookController.get_all_books)
 router.get('/get_book/:id', BookController.get_book)
+router.put('/update_book/:id', BookController.update_book)
 
 export default router
